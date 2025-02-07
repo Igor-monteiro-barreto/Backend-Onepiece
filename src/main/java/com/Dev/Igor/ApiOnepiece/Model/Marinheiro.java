bb68bde6b6d30333a6f6cd0f353e2b3de61d45e5
Igor Monteiro <igorbarreto.ti@outlook.com>
2025-02-07 16:59:13 -0300

    Create: Criação do model/entity e adicionamento da dependencia do flyway

--- src/main/java/com/Dev/Igor/ApiOnepiece/Model/Marinheiro.java
+++ src/main/java/com/Dev/Igor/ApiOnepiece/Model/Marinheiro.java
@@ -1,4 +1,55 @@
 package com.Dev.Igor.ApiOnepiece.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Marinheiros")
 public class Marinheiro {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int idade;
+    private String cargo;
+    private boolean Akumado;
+
+    public boolean isAkumado() {
+        return Akumado;
+    }
+
+    public void setAkumado(boolean akumado) {
+        Akumado = akumado;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
