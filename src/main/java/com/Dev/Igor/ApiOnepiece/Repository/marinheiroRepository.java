2abaca809cd019c7cf45ee9623f4e5ed87d615ad
Igor Monteiro <igorbarreto.ti@outlook.com>
2025-02-07 17:00:50 -0300

    Create: Criação do Repository

---
+++ src/main/java/com/Dev/Igor/ApiOnepiece/Repository/marinheiroRepository.java
@@ -0,0 +1,7 @@
+package com.Dev.Igor.ApiOnepiece.Repository;
+
+import com.Dev.Igor.ApiOnepiece.Model.Marinheiro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface marinheiroRepository extends JpaRepository<Marinheiro, Long> {
+}
