9540a249c42e8865c91f6cc4fd2612b0d3b62191
Igor Monteiro <igorbarreto.ti@outlook.com>
2025-02-18 17:31:16 -0300

    fix: Adicionei o Enum no lugar do String para o atributo cargo e agora o flyway não esta atualizando

---
+++ src/main/java/com/Dev/Igor/ApiOnepiece/Model/Cargo.java
@@ -0,0 +1,13 @@
+package com.Dev.Igor.ApiOnepiece.Model;
+
+public enum Cargo {
+    Almirante,
+    Vice_Almirante,
+    Capitão,
+    Recruta,
+    Almirante_de_Frota,
+    vice_capitão,
+    Contra_Almirante,
+    Aprendiz_de_Marinheiro,
+    Tenete
+}
