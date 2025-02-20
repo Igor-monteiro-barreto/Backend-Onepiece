0fce51834b88322a861cae02e1560d07f8e7ce8e
Igor Monteiro <igorbarreto.ti@outlook.com>
2025-02-20 18:10:57 -0300

    fix: Migration corrigida do flyway

--- src/main/resources/db/migration/V1_create-marinheiro.sql
+++ src/main/resources/db/migration/V1_create-marinheiro.sql
@@ -2,6 +2,8 @@ CREATE TABLE marinheiro(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(77) NOT NULL,
     idade INT NOT NULL,
-    cargo VARCHAR(77) NOT NULL,
+    cargo ENUM('Almirante', 'Vice_Almirante', 'Capitão', 'Recruta',
+     'Almirante_de_Frota', 'vice_capitão',
+     'Contra_Almirante', 'Aprendiz_de_Marinheiro', 'Tenente') NOT NULL,
     Akumado BOOLEAN NOT NULL
 );
